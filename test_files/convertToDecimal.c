768cf4461a0f8ecf98217c979f3d53a8b95f4454
DBsee1 <dominic.bassee.xf@gmail.com>
2023-01-23 03:36:01 -0800

    CD_Team_work [File name changed]

--- test_files/convertToDecimal.c
+++ test_files/convertToDecimal.c
@@ -7,16 +7,16 @@
  */
 long long convertToDecimal(int Number, int base)
 {
-    int decimal_number = 0, i = 0;
+    int decimalNumber = 0, i = 0;
 
     while(Number != 0)
     {
-        decimal_number += (Number%10) * power(base,i);
+        decimalNumber += (Number%10) * power(base,i);
         ++i;
         Number/=10;
     }
 
     i = 1;
 
-    return decimal_number;
+    return decimalNumber;
 }
--- test_files/print_binary.c
+++ test_files/print_binary.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 int print_binary(int number){
-    int array[32], i = 0, count = 0;
+    int arr[32], i = 0, count = 0;
     
     while (number >= 2){
-        array[i] = number%2;
+        arr[i] = number%2;
         number /= 2;
         i++;
     }
-    array[i] = number;
+    arr[i] = number;
     
     
     for(i; i >= 0; i--){
-        count += _putchar(array[i] + '0');
+        count += _putchar(arr[i] + '0');
     }
 
     return (count);
