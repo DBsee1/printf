5755986788e383effa101241cde2b7b00a53e75a
DBsee1 <dominic.bassee.xf@gmail.com>
2023-01-23 01:25:09 -0800

    CD_team_work 'source code refactored'

--- CD_functional_printf/get_print.c
+++ CD_functional_printf/get_print.c
@@ -29,11 +29,11 @@ int (*get_print(char s))(va_list, flags_t *)
 		{'p', print_address},
 		{'%', print_percent}
 		};
-	int flags = 14;
+	int size = 14;
 
 	register int i;
 
-	for (i = 0; i < flags; i++)
+	for (i = 0; i < size; i++)
 		if (func_arr[i].c == s)
 			return (func_arr[i].f);
 	return (NULL);
--- CD_test/convertToDecimal.c
+++ CD_test/convertToDecimal.c
@@ -7,16 +7,16 @@
  */
 long long convertToDecimal(int Number, int base)
 {
-    int decimalNumber = 0, i = 0;
+    int decimal_number = 0, i = 0;
 
     while(Number != 0)
     {
-        decimalNumber += (Number%10) * power(base,i);
+        decimal_number += (Number%10) * power(base,i);
         ++i;
         Number/=10;
     }
 
     i = 1;
 
-    return decimalNumber;
+    return decimal_number;
 }
